958607cfde3b6e52d423e0ac74b2fc77fba4d377
Sachin rajak <144539040+sachinrajak03@users.noreply.github.com>
2024-04-13 15:19:48 +0530

    Add files via upload

---
+++ merge sorted order ( leetcode 88).cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int k = m;
+        for (int i = 0; i < n; i++) {
+            nums1[k] = nums2[i];
+            k++;
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+};
